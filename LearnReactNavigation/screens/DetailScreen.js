315acbe564d37805e96a9ad3de08c4e8cb0a22fe
lee-youn <5816654@naver.com>
2024-01-24 21:21:13 +0900

    5장. 리액트 내비게이션

---
+++ LearnReactNavigation/screens/DetailScreen.js
@@ -0,0 +1,13 @@
+/* eslint-disable prettier/prettier */
+import React from 'react';
+import {View, Text} from 'react-native';
+
+function DetailScreen() {
+    return (
+        <View>
+            <Text>Detail</Text>
+        </View>
+    );
+}
+
+export default DetailScreen;
---
+++ LearnReactNavigation/screens/HomeScreen.js
@@ -0,0 +1,16 @@
+/* eslint-disable prettier/prettier */
+import React from 'react';
+import {View, Button} from 'react-native';
+
+function HomeScreen({navigation}) {
+    return (
+        <View>
+            <Button
+                title="Detail 열기"
+                onPress={() => navigation.navigate('Detail')}
+            />
+        </View>
+    );
+}
+
+export default HomeScreen;
